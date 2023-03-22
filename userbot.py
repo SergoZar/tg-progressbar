55d218dc47d83e355b31ec12c8c99a2ce28400ec
SergoZar <67230600+SergoZar@users.noreply.github.com>
2023-03-22 21:37:37 +0200

    Update userbot.py

--- userbot.py
+++ userbot.py
@@ -1,12 +1,5 @@
 #! /usr/bin/env python3
 # -*- coding: utf-8 -*-
-
-import requests
-
-r = requests.get("http://ddg.gg")
-print(r.text)
-
-'''
 import logging
 from pyrogram import Client, filters
 from pyrogram.errors import PeerIdInvalid
@@ -16,22 +9,12 @@ logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s
 
 app = Client("my_account", api_id = 1468033, api_hash = "9bd06a9bd53cee5ace0b84d25ed582ad")
 
-hello_text = """
-Вітаю новоприбулих! 
-Рекомендую подивитись закріплені повідомлення та полистати пости вгору. 
-Ця група містить матеріали вступу ще з 2020-го року - тобто три вступні 
-кампанії: 2020, 2021, 2022 років. Тому можна почитати багато матеріалів 
-про навчання на 121 спеціальності, про прохідний бад на контракт і бюджет,  
-про комп техніку та аудиторії, про гуртожиток, про різні event-и та почерпнути 
-багато іншої корисної для вступника інформації
-"""
-
-@app.on_message(filters.chat("@pzvntu2020") & filters.new_chat_members)
+@app.on_message(filters.chat("@me"))
 def welcome(app, message):
     try:
-        app.send_message(chat_id=message.chat.id, text=hello_text)
+        app.send_message(chat_id=message.chat.id, text=message.text)
     except PeerIdInvalid:
         pass
 
 app.run()
-'''
+
