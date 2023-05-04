a18ebb0f1c2fe04a45a1974dbc510e30001f8e6b
SergoZar <67230600+SergoZar@users.noreply.github.com>
2023-05-05 00:09:31 +0300

    Update userbot.py

--- userbot.py
+++ userbot.py
@@ -7,7 +7,7 @@ from pyrogram.errors import PeerIdInvalid
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
                      level=logging.INFO)
 
-app = Client("my_account", api_id = 1468033, api_hash = "9bd06a9bd53cee5ace0b84d25ed582ad")
+app = Client("my_account", api_id = 1, api_hash = "")
 
 @app.on_message(filters.chat("@me"))
 def welcome(app, message):
